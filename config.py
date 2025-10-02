298d90979e9ac8687770e1cd265d855552e6573f
Dulateaad <asdfsdassdsdd@gmail.com>
2025-10-03 00:30:14 +0500

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0")) 
